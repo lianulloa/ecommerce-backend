a133c096541783ea5715d58297da1aa7073284c1
Lian Ulloa <lianulloa808@gmail.com>
2021-10-20 16:23:04 -0400

    Create ListAPIViewWithMetadata and ModelViewSetWithMetadata

--- enterprise/views.py
+++ enterprise/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 import logging
 
-from rest_framework import viewsets, permissions, mixins, generics, filters
+from rest_framework import filters
 from rest_framework.exceptions import ValidationError
 
 from .serializers import *
 from .decorators.views import withMetadata
+from .drf_custom.generics import ListAPIViewWithMetadata
+from .drf_custom.viewsets import ModelViewSetWithMetadata
 # from .models import *
 
 logger = logging.getLogger("mfc")
-class EnterpriseViewSet(viewsets.ModelViewSet):
+class EnterpriseViewSet(ModelViewSetWithMetadata):
   queryset = Enterprise.objects.all()
   serializer_class = EnterpriseSerializer
 
   def perform_create(self, serializer):
     serializer.save(owner=self.request.user.customuser)
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductViewSet(ModelViewSetWithMetadata):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
   filter_backends = [filters.SearchFilter]
@@ -27,28 +29,20 @@ class ProductViewSet(viewsets.ModelViewSet):
       raise ValidationError('Only the owner of an enterprise can delete a product of it')
     instance.delete()
 
-class RatingViewSet(viewsets.ModelViewSet):
+class RatingViewSet(ModelViewSetWithMetadata):
   queryset = Rating.objects.all()
   serializer_class = RatingSerializer
 
   def perform_create(self, serializer):
     serializer.save(user=self.request.user.customuser)
 
-class CategoryListView(generics.ListAPIView):
+class CategoryListView(ListAPIViewWithMetadata):
   queryset = Category.objects.all()
   serializer_class = CategorySerializer
 
-  @withMetadata
-  def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
-
-class SubcategoryListView(mixins.ListModelMixin, generics.GenericAPIView):
+class SubcategoryListView(ListAPIViewWithMetadata):
   queryset = Category.objects.all()
   serializer_class = CategorySerializer
 
   def get_queryset(self):
     return Category.objects.get(id=self.kwargs['pk']).subcategory_set
-
-  @withMetadata
-  def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
